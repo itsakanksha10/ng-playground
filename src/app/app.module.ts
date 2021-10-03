1f1c5c64da8ab6c60aaa19e17f051bbb1faf06e4
Akanksha Shrivastav <itsakanksha10@gmail.com>
2021-10-03 03:29:02 -0700

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { DashboardComponent } from './user/dashboard/dashboard.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    DashboardComponent,
+    ProfileComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
